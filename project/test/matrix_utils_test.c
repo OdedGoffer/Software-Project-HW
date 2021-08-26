31f58428520d9e2bf9a118d770db600209282406
Oded Goffer <odedgoffer@mail.tau.ac.il>
2021-08-26 14:37:03 +0300

    matrix_utils tests

--- project/test/matrix_utils_test.c
+++ project/test/matrix_utils_test.c
@@ -54,11 +54,119 @@ void TEST_matrix_add_row() {
 
 }
 
+void TEST_matrix_diagonal_pow() {
+	matrix* mat;
+	vector* v;
+	vector* tmp;
+	int n = 10;
+	int i;
+
+	mat = matrix_init(n, 0);
+
+	for (i=0; i<n; i++) {
+		v->values[i] = 4;
+		tmp = vector_copy(v);
+		matrix_add_row(mat, v);
+		v = tmp;
+	}
+
+	matrix_diagonal_pow(mat, -2);
+
+	for (i=0; i<n; i++) {
+		assertf(mat->row[i]->values[i] == 0.5, "wrong number on diagonal");
+	}
+
+	vector_free(tmp);
+	matrix_free(mat);
+
+}
+
+void TEST_matrix_subtract() {
+	matrix* mat1;
+	matrix* mat2;
+	vector* v;
+	vector* tmp;
+	int n = 10;
+	int i;
+	int j;
+
+	mat1 = matrix_init(n, 0);
+	mat2 = matrix_init(n, 0);
+	v = vector_init_zero(n);
+	for (i=0; i<n; i++) {
+		v->values[i] = i;
+	}
+
+	for (i=0; i<n; i++) {
+		tmp = vector_copy(v);
+		matrix_add_row(mat1, v);
+		v = tmp;
+		tmp = vector_copy(v);
+		matrix_add_row(mat2, v);
+		v = tmp;
+	}
+
+	mat1 = matrix_subtract(mat1, mat2);
+
+	for (i=0; i<n; i++) {
+		for (j=0; j<n; j++) {
+			assertf(mat1->rows[i]->values[j] == 0, "problema amigo");
+		}
+	}
+
+	vector_free(tmp);
+	matrix_free(mat1);
+	matrix_free(mat2);
+}
+
+void TEST_matrix_mult() {
+	matrix* mat1;
+	matrix* mat2;
+	vector* v1;
+	vector* v2;
+	vector* tmp;
+	int n = 10;
+	int i;
+	int j;
+
+	mat1 = matrix_init(n, 0);
+	mat2 = matrix_init(n, 0);
+	v1 = vector_init_zero(n);
+	v2 = vector_init_zero(n);
+	for (i=0; i<n; i++) {
+		v1->values[i] = i;
+	}
+
+	for (i=0; i<n; i++) {
+		tmp = vector_copy(v1);
+		matrix_add_row(mat1, v1);
+		v1 = tmp;
+		tmp = vector_copy(v2);
+		matrix_add_row(mat2, v2);
+		v2 = tmp;
+	}
+
+	mat1 = matrix_mult(mat1, mat2);
+
+	for (i=0; i<n; i++) {
+		for (j=0; j<n; j++) {
+			assertf(mat1->rows[i]->values[j] == 0, "problema amigo");
+		}
+	}
+
+	vector_free(tmp);
+	matrix_free(mat1);
+	matrix_free(mat2);
+}
+
 int main() {
 	start_test(TEST);
 
 	TEST_matrix_init();
 	TEST_matrix_add_row();
+	TEST_matrix_diagonal_pow();
+	TEST_matrix_subtract();
+	TEST_matrix_mult();
 
 	end_test(TEST);
 }
